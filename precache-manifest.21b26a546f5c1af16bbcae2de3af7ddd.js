a522437b788d656c5f5b941d3e778ac9e18b0e4e
EF <ef@EFs-MacBook-Pro.local>
2019-01-18 14:34:51 -0500

    Updates

---
+++ precache-manifest.21b26a546f5c1af16bbcae2de3af7ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbc8781a3dc8a61b433887de4fbddd25",
+    "url": "/React-Clicky-Game-New/static/media/abstract-art-background-430207.fbc8781a.jpg"
+  },
+  {
+    "revision": "0108d7f584538a079e14",
+    "url": "/React-Clicky-Game-New/static/js/runtime~main.0108d7f5.js"
+  },
+  {
+    "revision": "38e4f3904cc562208f05",
+    "url": "/React-Clicky-Game-New/static/js/main.38e4f390.chunk.js"
+  },
+  {
+    "revision": "c93ee75f875a2756abf2",
+    "url": "/React-Clicky-Game-New/static/js/1.c93ee75f.chunk.js"
+  },
+  {
+    "revision": "38e4f3904cc562208f05",
+    "url": "/React-Clicky-Game-New/static/css/main.7fdbef31.chunk.css"
+  },
+  {
+    "revision": "3bef8c295fb001946b12eae299066bf2",
+    "url": "/React-Clicky-Game-New/index.html"
+  }
+];
